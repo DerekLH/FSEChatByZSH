2d3744e9f19a8b329b69a47b213b565b68d22f2f
Shuhao Zhang <Shuhao.Zhang@sv.cmu.edu>
2016-08-28 23:42:58 -0700

    finish all

--- newChatRoom/index.js
+++ newChatRoom/index.js
@@ -2,15 +2,41 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'fse'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+// view engine setup
+app.set('views', __dirname);
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+
+var ChatRoomPage = require('./ChatRoomPage');
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
-app.get('/ChatRoomPage', function(req, res){
-  res.sendFile(__dirname + '/ChatRoomPage.html');
-});
+
+
+app.use('/ChatRoomPage',ChatRoomPage);
+
+//app.get('/ChatRoomPage', function(req, res){
+//  res.sendFile(__dirname + '/ChatRoomPage.html');
+//});
 // DB Schema:
 // create table Message(content varchar(1000));
 io.on('connection', function(socket){
@@ -20,7 +46,16 @@ io.on('connection', function(socket){
   });
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
-    io.emit('chat message',msg);
+    var insertSQL = 'INSERT INTO Message(content) VALUES(?)';
+    connection.query(insertSQL,msg,function(err,result){
+    	if(err)
+    	{
+    		console.log('INSERT ERR',err.message);
+    		return;
+    	}
+    	io.emit('chat message',msg);
+
+    });
   });
 });
 
