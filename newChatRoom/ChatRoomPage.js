4abeaf2766c3d4629ba229c3b9a7a471440e8038
Shuhao Zhang <Shuhao.Zhang@sv.cmu.edu>
2016-09-04 11:01:57 -0700

    a new bug fixed

--- newChatRoom/ChatRoomPage.js
+++ newChatRoom/ChatRoomPage.js
@@ -2,27 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'fse'
-});
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
 
-  console.log('connected as id ' + connection.threadId);
-});
+
 
 var history = new Array();
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+	var connection = mysql.createConnection({
+ 	host     : 'localhost',
+ 	user     : 'root',
+  	password : '',
+  	database : 'fse'
+	});
+	connection.connect(function(err) {
+  	if (err) {
+   	 	console.error('error connecting: ' + err.stack);
+    	return;
+  	}
+
+  	console.log('connected as id ' + connection.threadId);
+
 	connection.query('SELECT * FROM Message', function(err, result, fields){
 	var len = result.length ;
 	//console.log(len);
@@ -35,8 +38,14 @@ router.get('/', function(req, res, next) {
 	}
 	//history = history + "'" + result[len-1].content + "']";
 	//console.log(history);
-});
-  res.render('ChatRoomPage',{his:history});
+	console.log("here\n");
+	console.log(history);
+  	res.render('ChatRoomPage',{his:history});
+	});
+	
+	});
+
+
 });
 
 module.exports = router;
